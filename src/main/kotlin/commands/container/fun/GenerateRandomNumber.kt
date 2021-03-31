e52dfa1de8f79150d2e268a15e9276537fe52db1
stonebank <hassan_99@live.dk>
2021-03-31 18:36:54 +0200

    Added a useless command

--- src/main/kotlin/commands/container/fun/GenerateRandomNumber.kt
+++ src/main/kotlin/commands/container/fun/GenerateRandomNumber.kt
@@ -7,8 +7,28 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 @CommandInfo(cmd = "rng,randomnumber,roll", description = "Generates a random number between 1-100")
 class GenerateRandomNumber : CommandHandler() {
 
-    override fun execute(event: MessageReceivedEvent, vararg cmd: String?) {
-        event.channel.sendMessage("You rolled ${(1..100).random()}!").queue()
+    override fun execute(event: MessageReceivedEvent, vararg cmd: String) {
+
+        if (cmd[1].toIntOrNull() == null) {
+            event.channel.sendMessage("Your first argument must be an integer.").queue()
+            return
+        }
+
+        val min = cmd[1].toInt()
+
+        if (cmd[2].toIntOrNull() == null) {
+            event.channel.sendMessage("Your second argument must be an integer.").queue()
+            return
+        }
+
+        val max = cmd[2].toInt()
+
+        if (min > max) {
+            event.channel.sendMessage("The maximum input ($max) has to be higher than the lower input ($min).").queue()
+            return
+        }
+
+        event.channel.sendMessage("You rolled ${(min..max).random()}!").queue()
     }
 
 }
